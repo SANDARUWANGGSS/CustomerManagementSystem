7eaef433b25b3d720b8ee5271d8a8497bfc89aba
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-05-04 18:19:23 +0530

    Completed add customer

---
+++ src/main/java/com/customer/CustomerManagement/repo/CustomerRepo.java
@@ -0,0 +1,13 @@
+package com.customer.CustomerManagement.repo;
+
+import com.customer.CustomerManagement.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface CustomerRepo extends JpaRepository<Customer,Integer>
+{
+
+}
--- src/main/java/com/customer/CustomerManagement/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/customer/CustomerManagement/service/impl/CustomerServiceImpl.java
@@ -1,16 +1,30 @@
 package com.customer.CustomerManagement.service.impl;
 
 import com.customer.CustomerManagement.dto.CustomerDTO;
+import com.customer.CustomerManagement.entity.Customer;
+import com.customer.CustomerManagement.repo.CustomerRepo;
 import com.customer.CustomerManagement.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService
 {
+    @Autowired
+    private CustomerRepo customerRepo;
     @Override
     public String saveCustomer(CustomerDTO customerDTO)
     {
+        Customer customer=new Customer(
+                customerDTO.getCustomerId(),
+                customerDTO.getCustomerName(),
+                customerDTO.getCustomerAddress(),
+                customerDTO.getCustomerSalary(),
+                customerDTO.getNic(),
+                customerDTO.isActiveStatus()
+        );
         System.out.println("Customer Service "+customerDTO);
+        customerRepo.save(customer);
         return "saved";
     }
 
